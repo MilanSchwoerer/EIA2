a96f7e12cff41c1bfd04e86b72f8f003aec1313e
user <user@Milan-Laptop.fritz.box>
2018-11-12 11:41:06 +0100

    Update

--- Aufgabe3/js/Aufgabe3.ts
+++ Aufgabe3/js/Aufgabe3.ts
@@ -1,512 +1,111 @@
-namespace Aufgabe2new {
-    document.addEventListener("DOMContentLoaded", uno)
+namespace Aufgabe3 {
+    document.addEventListener("DOMContentLoaded", Anzahl);
+    document.addEventListener("keypress", pressKeyboard);
 
-    function uno() {
 
-        let cardNumber: number;
-        let ChooseCards: string = prompt("Anzahl der gew√ºnschten Handkarten eingeben");
-        cardNumber = Number(ChooseCards);
+    interface card {
+        color: string;
+        value: string;
+    }
 
-        interface Card {
-            color: string;
-            value: string;
-        }
-        let b1 = {
-            color: "blue",
-            value: "0",
-        };
-        let b2 = {
-            color: "blue",
-            value: "1",
-        };
-        let b3 = {
-            color: "blue",
-            value: "2",
-        };
-        let b4 = {
-            color: "blue",
-            value: "3",
-        };
-        let b5 = {
-            color: "blue",
-            value: "4",
-        };
-        let b6 = {
-            color: "blue",
-            value: "5",
-        };
-        let b7 = {
-            color: "blue",
-            value: "6",
-        };
-        let b8 = {
-            color: "blue",
-            value: "7",
-        };
-        let b9 = {
-            color: "blue",
-            value: "8",
-        };
-        let b10 = {
-            color: "blue",
-            value: "9",
-        };
-        let b11 = {
-            color: "blue",
-            value: "take2"
-        };
-        let b12 = {
-            color: "blue",
-            value: "reverse",
-        };
-        let b13 = {
-            color: "blue",
-            value: "missturn",
-        };
-        let b14 = {
-            color: "blue",
-            value: "1",
-        };
-        let b15 = {
-            color: "blue",
-            value: "2",
-        };
-        let b16 = {
-            color: "blue",
-            value: "3",
-        };
-        let b17 = {
-            color: "blue",
-            value: "4",
-        };
-        let b18 = {
-            color: "blue",
-            value: "5",
-        };
-        let b19 = {
-            color: "blue",
-            value: "6",
-        };
-        let b20 = {
-            color: "blue",
-            value: "7",
-        };
-        let b21 = {
-            color: "blue",
-            value: "8",
-        };
-        let b22 = {
-            color: "blue",
-            value: "9",
-        };
-        let b23 = {
-            color: "blue",
-            value: "take2"
-        };
-        let b24 = {
-            color: "blue",
-            value: "reverse",
-        };
-        let b25 = {
-            color: "blue",
-            value: "missturn",
-        };
-        let r1 = {
-            color: "red",
-            value: "0",
-        };
-        let r2 = {
-            color: "red",
-            value: "1",
-        };
-        let r3 = {
-            color: "red",
-            value: "2",
-        };
-        let r4 = {
-            color: "red",
-            value: "3",
-        };
-        let r5 = {
-            color: "red",
-            value: "4",
-        };
-        let r6 = {
-            color: "red",
-            value: "5",
-        };
-        let r7 = {
-            color: "red",
-            value: "6",
-        };
-        let r8 = {
-            color: "red",
-            value: "7",
-        };
-        let r9 = {
-            color: "red",
-            value: "8",
-        };
-        let r10 = {
-            color: "red",
-            value: "9",
-        };
-        let r11 = {
-            color: "red",
-            value: "take2"
-        };
-        let r12 = {
-            color: "red",
-            value: "reverse",
-        };
-        let r13 = {
-            color: "red",
-            value: "missturn",
-        };
-        let r14 = {
-            color: "red",
-            value: "1",
-        };
-        let r15 = {
-            color: "red",
-            value: "2",
-        };
-        let r16 = {
-            color: "red",
-            value: "3",
-        };
-        let r17 = {
-            color: "red",
-            value: "4",
-        };
-        let r18 = {
-            color: "red",
-            value: "5",
-        };
-        let r19 = {
-            color: "red",
-            value: "6",
-        };
-        let r20 = {
-            color: "red",
-            value: "7",
-        };
-        let r21 = {
-            color: "red",
-            value: "8",
-        };
-        let r22 = {
-            color: "red",
-            value: "9",
-        };
-        let r23 = {
-            color: "red",
-            value: "take2"
-        };
-        let r24 = {
-            color: "red",
-            value: "reverse",
-        };
-        let r25 = {
-            color: "red",
-            value: "missturn",
-        };
-        let y1 = {
-            color: "yellow",
-            value: "0",
-        };
-        let y2 = {
-            color: "yellow",
-            value: "1",
-        };
-        let y3 = {
-            color: "yellow",
-            value: "2",
-        };
-        let y4 = {
-            color: "yellow",
-            value: "3",
-        };
-        let y5 = {
-            color: "yellow",
-            value: "4",
-        };
-        let y6 = {
-            color: "yellow",
-            value: "5",
-        };
-        let y7 = {
-            color: "yellow",
-            value: "6",
-        };
-        let y8 = {
-            color: "yellow",
-            value: "7",
-        };
-        let y9 = {
-            color: "yellow",
-            value: "8",
-        };
-        let y10 = {
-            color: "yellow",
-            value: "9",
-        };
-        let y11 = {
-            color: "yellow",
-            value: "take2"
-        };
-        let y12 = {
-            color: "yellow",
-            value: "reverse",
-        };
-        let y13 = {
-            color: "yellow",
-            value: "missturn",
-        };
-        let y14 = {
-            color: "yellow",
-            value: "1",
-        };
-        let y15 = {
-            color: "yellow",
-            value: "2",
-        };
-        let y16 = {
-            color: "yellow",
-            value: "3",
-        };
-        let y17 = {
-            color: "yellow",
-            value: "4",
-        };
-        let y18 = {
-            color: "yellow",
-            value: "5",
-        };
-        let y19 = {
-            color: "yellow",
-            value: "6",
-        };
-        let y20 = {
-            color: "yellow",
-            value: "7",
-        };
-        let y21 = {
-            color: "yellow",
-            value: "8",
-        };
-        let y22 = {
-            color: "yellow",
-            value: "9",
-        };
-        let y23 = {
-            color: "yellow",
-            value: "take2"
-        };
-        let y24 = {
-            color: "yellow",
-            value: "reverse",
-        };
-        let y25 = {
-            color: "yellow",
-            value: "missturn",
-        };
-        let g1 = {
-            color: "green",
-            value: "0",
-        };
-        let g2 = {
-            color: "green",
-            value: "1",
-        };
-        let g3 = {
-            color: "green",
-            value: "2",
-        };
-        let g4 = {
-            color: "green",
-            value: "3",
-        };
-        let g5 = {
-            color: "green",
-            value: "4",
-        };
-        let g6 = {
-            color: "green",
-            value: "5",
-        };
-        let g7 = {
-            color: "green",
-            value: "6",
-        };
-        let g8 = {
-            color: "green",
-            value: "7",
-        };
-        let g9 = {
-            color: "green",
-            value: "8",
-        };
-        let g10 = {
-            color: "green",
-            value: "9",
-        };
-        let g11 = {
-            color: "green",
-            value: "take2"
-        };
-        let g12 = {
-            color: "green",
-            value: "reverse",
-        };
-        let g13 = {
-            color: "green",
-            value: "missturn",
-        };
-        let g14 = {
-            color: "green",
-            value: "1",
-        };
-        let g15 = {
-            color: "green",
-            value: "2",
-        };
-        let g16 = {
-            color: "green",
-            value: "3",
-        };
-        let g17 = {
-            color: "green",
-            value: "4",
-        };
-        let g18 = {
-            color: "green",
-            value: "5",
-        };
-        let g19 = {
-            color: "green",
-            value: "6",
-        };
-        let g20 = {
-            color: "green",
-            value: "7",
-        };
-        let g21 = {
-            color: "green",
-            value: "8",
-        };
-        let g22 = {
-            color: "green",
-            value: "9",
-        };
-        let g23 = {
-            color: "green",
-            value: "take2"
-        };
-        let g24 = {
-            color: "green",
-            value: "reverse",
-        };
-        let g25 = {
-            color: "green",
-            value: "missturn",
-        };
-        let k1 = {
-            color: "black",
-            value: "Farbwunsch",
-        };
-        let k2 = {
-            color: "black",
-            value: "Farbwunsch",
-        };
-        let k3 = {
-            color: "black",
-            value: "Farbwunsch",
-        };
-        let k4 = {
-            color: "black",
-            value: "Farbwunsch",
-        };
-        let k5 = {
-            color: "black",
-            value: "+4",
-        };
-        let k6 = {
-            color: "black",
-            value: "+4",
-        };
-        let k7 = {
-            color: "black",
-            value: "+4",
-        };
-        let k8 = {
-            color: "black",
-            value: "+4",
-        };
-        let deck = [r1, r2, r3, r4, r5, r6, r7, r8, r9, r10, r11, r12, r13, r14, r15, r16, r17, r18, r19, r20, r21, r22, r23, r24, r25,
-            b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23, b24, b25,
-            g1, g2, g3, g4, g5, g6, g7, g8, g9, g10, g11, g12, g13, g14, g15, g16, g17, g18, g19, g20, g21, g22, g23, g24, g25,
-            y1, y2, y3, y4, y5, y6, y7, y8, y9, y10, y11, y12, y13, y14, y15, y16, y17, y18, y19, y20, y21, y22, y23, y24, y25,
-            k1, k2, k3, k4, k5, k6, k7, k8];
-        for (let i = 0; i < cardNumber; i++) {
-            let randomCard = random(deck.length);
-            placeDiv(deck[randomCard].color, deck[randomCard].value, i);
-            deck.splice(randomCard, 1);
-            continue;
-        }
-        function random(Zufall: number) { return Math.floor(Math.random() * Math.floor(Zufall)) }
-
-        function placeDiv(_color: string, Karte: string, Eingabe: number): void {
-            let div: HTMLDivElement = document.createElement("div");
-            document.body.appendChild(div);
-            div.setAttribute("class", "cards");
-            div.setAttribute("id", "a" + Eingabe);
-            document.getElementById("a" + Eingabe).innerHTML += Karte;
-
-            let s: CSSStyleDeclaration = div.style;
-            s.border = "solid black";
-            s.position = "absolute";
-            s.backgroundColor = _color;
-            s.color = "white";
-            s.width = 150 + "px";
-            s.height = 250 + "px";
-            s.left = (Eingabe + 1) * 160 + "px";
-            s.bottom = 130 + "px";
-            s.textAlign = "center";
-            s.borderRadius = 20 + "px";
-        }
     
+    let colors: string[] = ["red", "blue", "green", "yellow"];
+    let values: string[] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "+2", "x", "<-->"];
+    let cards: card[] = [{ color: "red", value: "0" }, { color: "blue", value: "0" }, { color: "green", value: "0" }, { color: "yellow", value: "0" },
+        { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" }, { color: "black", value: "+4" },
+        { color: "black", value: "Wish" }, { color: "black", value: "Wish" }, { color: "black", value: "Wish" }, { color: "black", value: "Wish" }];
+    let place: card[] = [];
 
-    function take(_cards: number): void {
-        if (deck.length > 0) {
-            for (let i: number = 0; i < _cards; i++) {
-                let maxNumber: number = deck.length;
-                let randomNum: number = getRandom(maxNumber);
-                let : string = deck.splice(randomNum, 1)[0];
-                hand.push(card);
+    for (let i: number = 0; i < colors.length; i++) {
+        for (let a: number = 0; a < values.length; a++) {
+            for (let z: number = 0; z < 2; z++) {
+                let card: card = { color: colors[i], value: values[a] };
+                cards.push(card);
             }
         }
     }
+    console.log(cards);
 
-    function takeListener(): void {
-        document.getElementById("deck").addEventListener("click", takeEvent);
-        document.addEventListener("keydown", space);
+    let displayhand: card[] = [];
+
+    
+    function Anzahl(): void {
+        var cardNumber: string = prompt("Anzahl der gew¸nschten Handkarten eingeben");
+        let n: number = parseInt(cardNumber);
+
+        randomCard(n);
+        handcards();
+
+        let button: HTMLElement = document.getElementById("button");
+        button.addEventListener("click", sort);
+
+        let pull: HTMLElement = document.getElementById("draw");
+        pull.addEventListener("click", add);
     }
 
-    takeListener();
-    function takeEvent(): void {
-        take(1);
-        discard();
-        createCards(hand);
-        put();
-    }
-
-    function space(_event: KeyboardEvent): void {
-        var keyCode: number = _event.keyCode;
-        if (keyCode == 32) {
-            takeEvent();
+    function randomCard(n: number): void {
+        for (let random: number = n; random > 0; random--) {
+            let r: number = Math.floor(Math.random() * (cards.length - 1));
+            displayhand.push(cards[r]);
+            cards.splice(r, 1);
         }
     }
 
-}
-}
+    function add(): void {
+        randomCard(1);
+        handcards();
+    }
+
+    function pressKeyboard(_event: KeyboardEvent): void {
+        if (_event.keyCode == 32) {
+            add();
+        }
+    }
+
+    function handcards(): void {
+        document.getElementById("displayhand").innerHTML = "";
+        for (let d: number = 0; d < displayhand.length; d++) {
+            let div: HTMLElement = document.createElement("div");
+            document.getElementById("displayhand").appendChild(div);
+            div.innerHTML = displayhand[d].value;
+            div.classList.add(displayhand[d].color);
+            let id: string = d.toString();
+            div.setAttribute("id", id);
+            div.addEventListener("click", playCard);
+        }
+    }
+
+    function playCard(_event: MouseEvent): void {
+        document.getElementById("tray").innerHTML = "";
+        let clickedCard: HTMLElement = <HTMLElement>_event.target;
+        let index: number = parseInt(clickedCard.id);
+        place.push(displayhand[index]);
+        let div: HTMLElement = document.createElement("div");
+        document.getElementById("tray").appendChild(div);
+        div.innerHTML = displayhand[index].value;
+        div.classList.add(displayhand[index].color);
+        displayhand.splice(index, 1);
+        handcards();
+    }
+
+    function sort(): void {
+        document.getElementById("displayhand").innerHTML = "";
+        displayhand.sort(function(a: card, b: card): number {
+            if (a.color > b.color) {
+                return 1;
+            }
+            if (a.color < b.color) {
+                return -1;
+            }
+            if (a.value > b.value) {
+                return 1;
+            }
+            if (a.value < b.value) {
+                return -1;
+            }
+            return 0;
+        });
+
+        console.log(displayhand);
+        handcards();
+    } }
