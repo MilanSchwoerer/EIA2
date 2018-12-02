bd37d06aa30855e2b024ca0dca906c9b6bb4607e
user <user@Milan-Laptop.fritz.box>
2018-12-02 18:08:10 +0100

    Aufgabe 6

---
+++ Aufgabe6/js/A6productArray.js
@@ -0,0 +1,33 @@
+var Aufgabe6;
+(function (Aufgabe6) {
+    Aufgabe6.data = {
+        "trees": [
+            { name: "Nordmantanne (50 Euro)", price: 50 },
+            { name: "Fichte (40 Euro)", price: 40 }
+        ],
+        "christmasBalls": [
+            { name: "Rot 10 Stück (5 Euro)", price: 5 },
+            { name: "Gold 10 Stück (10 Euro)", price: 10 },
+            { name: "Grün 10 Stück (4 Euro)", price: 4 }
+        ],
+        "tinsels": [
+            { name: "Lametta Gold 50cm (3 Euro)", price: 3 },
+            { name: "Lametta Silber 50cm (2 Euro)", price: 2 },
+            { name: "Lametta Rot 50cm (3 Euro)", price: 3 }
+        ],
+        "candles": [
+            { name: "Kerzen rot (7 Euro)", price: 15 },
+            { name: "Kerzen gold (10 Euro)", price: 12 }
+        ],
+        "holder": [
+            { name: "Halterung Rot (20 Euro)", price: 20 },
+            { name: "Halterung Grün (22 Euro)", price: 22 }
+        ],
+        "delivery": [
+            { name: "UPS (15 Euro)", price: 15 },
+            { name: "DHL (12 Euro)", price: 12 },
+            { name: "DHL (14 Euro)", price: 14 }
+        ]
+    };
+})(Aufgabe6 || (Aufgabe6 = {}));
+//# sourceMappingURL=A6productArray.js.map
---
+++ Aufgabe6/js/Aufgabe6.js
@@ -0,0 +1,169 @@
+var Aufgabe6;
+(function (Aufgabe6) {
+    window.addEventListener("load", init);
+    let treeboolean = false;
+    let holderboolean = false;
+    let deliveryboolean = false;
+    function init() {
+        console.log("start");
+        displayFieldsets(Aufgabe6.data);
+        document.getElementsByTagName("body")[0].addEventListener("input", handleChange);
+        document.getElementById("check").addEventListener("click", check);
+    }
+    function displayFieldsets(_item) {
+        let form = document.createElement("form");
+        form.setAttribute("action", "https://eia2-server-node.herokuapp.com/");
+        form.setAttribute("method", "get");
+        document.getElementsByTagName("body")[0].appendChild(form);
+        for (let key in _item) {
+            console.log(key);
+            let value = _item[key];
+            let h2 = document.createElement("h2");
+            form.appendChild(h2);
+            h2.innerText = key;
+            let fieldset = document.createElement("fieldset");
+            form.appendChild(fieldset);
+            fieldset.setAttribute("name", key);
+            fieldset.setAttribute("id", key);
+            for (let i = 0; i < value.length; i++) {
+                createInnerFieldset(value[i], fieldset, key);
+            }
+        }
+        let h2 = document.createElement("h2");
+        form.appendChild(h2);
+        h2.innerHTML = "Anschrift";
+        let div = document.createElement("div");
+        let HTML = "<input class=adress type=text name=Text placeholder=Strasse required />";
+        HTML += "<input class=adress type=text name=Pattern pattern={1,} placeholder=Hausnummer required />";
+        form.appendChild(div);
+        div.innerHTML = HTML;
+        form.innerHTML += "<button type=submit>Submit</button>";
+    }
+    function createInnerFieldset(_heteroPredef, _fieldset, _key) {
+        if (_key == "tree" || _key == "holder" || _key == "shipment") {
+            let forID = _fieldset.childNodes.length;
+            let label = document.createElement("label");
+            _fieldset.appendChild(label);
+            label.setAttribute("for", _key + forID);
+            label.innerText = _heteroPredef.name;
+            let input = document.createElement("input");
+            label.appendChild(input);
+            input.setAttribute("price", _heteroPredef.price.toString());
+            input.setAttribute("type", "radio");
+            input.setAttribute("name", "radio" + _key);
+            input.setAttribute("id", _key + forID);
+            input.setAttribute("hiddenName", _heteroPredef.name);
+            input.setAttribute("category", _key);
+        }
+        else {
+            let p = document.createElement("p");
+            _fieldset.appendChild(p);
+            let input = document.createElement("input");
+            _fieldset.appendChild(input);
+            input.setAttribute("price", _heteroPredef.price.toString());
+            input.setAttribute("value", "0");
+            input.setAttribute("pattern", "[0-9]{1,}");
+            input.setAttribute("name", _key + _heteroPredef.name);
+            input.setAttribute("category", _key);
+            p.innerText = _heteroPredef.name;
+        }
+    }
+    function handleChange(_event) {
+        let target = _event.target;
+        target.setAttribute("value", target.value);
+        if (target.name == "radiotree") {
+            treeboolean = true;
+            for (let i = 0; i < Aufgabe6.data["tree"].length; i++) {
+                let dom = document.getElementById("tree" + i);
+                dom.setAttribute("hiddenvalue", "off");
+            }
+            target.setAttribute("hiddenvalue", "on");
+        }
+        if (target.name == "radioholder") {
+            holderboolean = true;
+            for (let i = 0; i < Aufgabe6.data["holder"].length; i++) {
+                let dom = document.getElementById("holder" + i);
+                dom.setAttribute("hiddenvalue", "off");
+            }
+            target.setAttribute("hiddenvalue", "on");
+        }
+        if (target.name == "radioshipment") {
+            deliveryboolean = true;
+            for (let i = 0; i < Aufgabe6.data["shipment"].length; i++) {
+                let dom = document.getElementById("shipment" + i);
+                dom.setAttribute("hiddenvalue", "off");
+            }
+            target.setAttribute("hiddenvalue", "on");
+        }
+        if (target.className == "adress" && target.value.length > 0) {
+            target.setAttribute("hiddenName", target.value);
+            target.setAttribute("hiddenvalue", "on");
+        }
+        let articles = document.getElementsByTagName("input");
+        let checkout = document.getElementById("checkout");
+        checkout.innerText = "";
+        for (let i = 0; i < articles.length; i++) {
+            let article = articles[i];
+            let articleName = article.getAttribute("name");
+            if (articleName == "radiotree" || articleName == "radioholder" || articleName == "radioshipment") {
+                articleName = article.getAttribute("hiddenName");
+            }
+            let articleValue = Number(article.getAttribute("value"));
+            let articlePrice = Number(article.getAttribute("price"));
+            if (articleValue > 0 || article.getAttribute("hiddenvalue") == "on") {
+                let articleCategory = article.getAttribute("category");
+                if (articleCategory == "tree" || articleCategory == "holder" || articleCategory == "shipment") {
+                    articleValue = 1;
+                }
+                let price = articleValue * articlePrice;
+                if (article.className == "adress") {
+                    if (article.name == "Text") {
+                        articleCategory = "Adresse:";
+                    }
+                    else {
+                        articleCategory = "Hausnummer:";
+                    }
+                    let createArticle = document.createElement("p");
+                    checkout.appendChild(createArticle);
+                    createArticle.innerText = articleCategory + article.getAttribute("hiddenName");
+                }
+                else {
+                    let createArticle = document.createElement("p");
+                    checkout.appendChild(createArticle);
+                    createArticle.setAttribute("price", price.toString());
+                    createArticle.innerText = articleCategory + ": " + articleName + " x " + articleValue;
+                }
+            }
+        }
+        calcPrice();
+    }
+    function calcPrice() {
+        let co = document.getElementById("checkout");
+        let gesPrice = 0;
+        for (let i = 0; i < co.childNodes.length; i++) {
+            let price = Number(document.getElementsByTagName("p")[i].getAttribute("price"));
+            gesPrice += price;
+            document.getElementById("price").innerText = gesPrice.toString();
+        }
+        console.log(gesPrice);
+    }
+    function check() {
+        let prompt = "Bitte noch wählen!:";
+        if (treeboolean == false || holderboolean == false || deliveryboolean == false) {
+            if (treeboolean == false) {
+                prompt += "Baum ";
+            }
+            if (holderboolean == false) {
+                prompt += "Halter ";
+            }
+            if (deliveryboolean == false) {
+                prompt += "Lieferung";
+            }
+            alert(prompt);
+        }
+        else {
+            alert("Alles ausgewählt!");
+        }
+    }
+})(Aufgabe6 || (Aufgabe6 = {}));
+//# sourceMappingURL=Aufgabe6.js.map
